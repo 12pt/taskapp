f0deceef9e2be1a0d9e56d6e4af2f58d56c26da0
12pt <12pt@users.noreply.github.com>
2018-02-03 17:48:31 +0000

    Flush out the tests.

--- test/APITest.php
+++ test/APITest.php
@@ -1,23 +1,50 @@
 <?php declare(strict_types=1);
 
 require_once dirname(__FILE__) . '/../api.php';
+require_once dirname(__FILE__) . '/../database.php';
 
 use PHPUnit\Framework\TestCase;
 
 class APITest extends TestCase {
+    private $db;
+
     function setUp() {
-        # api will not use a class.
+        $this->db = new Database("localhost", "taskapp_test", "app", "foobar");
     }
     
-    public function testGET() {
-        route("/api.php/", "GET");
+    function testGET() {
+        $result = route("/api.php/", "GET");
+        $this->assertJsonStringEqualsJsonString($result, $this->db->getAll());
     }
 
-    public function testPUT() {
-        route("/api.php/", "PUT");
+    function testPOST() {
+        # sketchy edit of POST but its for testing purposes and shouldn't affect production
+        $_POST["title"] = "my new title [" . rand() . "]";
+        $_POST["content"] = "my new content [" . rand() . "]";
+        $result = json_decode(route("/api.php/", "POST"), true);
+
+        $this->assertEquals($_POST["title"],
+                    $result["title"]);
+        $this->assertEquals($_POST["content"],
+                    $result["content"]);
     }
 
-    public function testDELETE() {
-        route("/api.php/", "DELETE");
+    function testPUT() {
+        # can't really test PUTting here
+    }
+
+    function testDELETE() {
+        # ensure a row exists, rather than selecting a random existing one
+        $_POST["title"] = "my new title [" . rand() . "]";
+        $_POST["content"] = "my new content [" . rand() . "]";
+        $result = json_decode(route("/api.php/", "POST"), true);
+
+        $id = $result["id"];
+        route("/api.php/$id", "DELETE");
+
+        $deletionCheck = $this->db->hasTask((string) $id);
+        $this->assertEquals(
+            json_decode($deletionCheck, true)["count"],
+            0);
     }
 }
